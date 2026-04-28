538fbafe4909bc20119496bffb9048cb6bd55a1f
karim-v <villalbaacevalk@gmail.com>
2026-04-28 22:38:23 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -63,3 +63,6 @@ USE_I18N = True
 USE_TZ = True
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Configuración para archivos estáticos (CSS, Imágenes)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
